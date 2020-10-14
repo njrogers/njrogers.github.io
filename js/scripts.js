323fcbb8db32533163bd712a531e68629acf752f
njrogers <60498789+njrogers@users.noreply.github.com>
2020-10-14 09:13:30 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@
                     {
                         scrollTop: target.offset().top,
                     },
-                    500,
+                    5000,
                     "easeInOutExpo"
                 );
                 return false;
